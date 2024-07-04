0a15aa5029b117804bbe68087751c6dd832280d0
CHOI HOWON <cheerhow@naver.com>
2024-07-05 00:04:55 +0900

    feat : 메인버튼 onClick함수 생성

--- src/components/MainButton.jsx
+++ src/components/MainButton.jsx
@@ -1,26 +1,21 @@
 /* eslint-disable react/prop-types */
 // src/components/StyledButton.jsx
 import React from 'react';
-import { Button } from '@mui/material';
+import styled from 'styled-components';
 
-const MainButton = ({ children }) => {
-  return (
-    <Button
-      variant="contained"
-      sx={{
-        background: 'linear-gradient(45deg, #b75dfd 30%, #ffa9a9 90%)',
-        borderRadius: '1rem',
-        width: '20rem',
-        height: '3rem',
-        fontSize: '1rem',
-        color: 'white',
-        textTransform: 'none',
-        marginTop: '1.5rem',
-      }}
-    >
-      {children}
-    </Button>
-  );
+const Button = styled.button`
+  background: linear-gradient(45deg, #b75dfd 30%, #ffa9a9 90%);
+  border-radius: 1rem;
+  width: 20rem;
+  height: 3rem;
+  font-size: 1rem;
+  color: white;
+  text-transform: none;
+  margin-top: 1.5rem;
+`;
+
+const MainButton = ({ onClick, children }) => {
+  return <Button onClick={onClick}>{children}</Button>;
 };
 
 export default MainButton;
