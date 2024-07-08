41ca19235e5cae853a860ef4c38905d07c128da8
Youth-one <o3her456@gmail.com>
2024-07-08 23:02:52 +0900

    design : Edit page 디자인 퍼블리싱 추후에 수정후 DB에 반영기능 수정 필

--- src/pages/AppRouter.jsx
+++ src/pages/AppRouter.jsx
@@ -4,6 +4,7 @@ import Login from './Login';
 import Mypage from './Mypage';
 import Search from './Search';
 import App from '../App';
+import EditProfile from './EditProfile';
 
 function AppRouter() {
   return (
@@ -13,6 +14,7 @@ function AppRouter() {
           <Route path="/" element={<Mypage />} />
           <Route path="/search" element={<Search />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/edit" element={<EditProfile />} />
         </Routes>
       </App>
     </Router>
