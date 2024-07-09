70ab66a24abbf25db5342941eb9beaa7aacc3bfd
CHOI HOWON <cheerhow@naver.com>
2024-07-09 16:25:18 +0900

    충돌조정

--- src/pages/AppRouter.jsx
+++ src/pages/AppRouter.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Main from './Main';
-import MainPage from './MainPage';
 
 function AppRouter() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/MainPage" element={<MainPage />} />
-      </Routes>
+      <Routes></Routes>
     </Router>
   );
 }
