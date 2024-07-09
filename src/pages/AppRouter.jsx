5feadf37ee8c2965d62189425c2060c731664a00
CHOI HOWON <cheerhow@naver.com>
2024-07-09 16:29:10 +0900

    충돌해결

--- src/pages/AppRouter.jsx
+++ src/pages/AppRouter.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './Login';
+import Mypage from './Mypage';
+import Search from './Search';
+import App from '../App';
+import Join from './Join';
 
 function AppRouter() {
-  return <Router></Router>;
+  return (
+    <Router>
+      <App>
+        <Routes>
+          <Route path="/" element={<Mypage />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/join" element={<Join />} />
+        </Routes>
+      </App>
+    </Router>
+  );
 }
 
 export default AppRouter;
