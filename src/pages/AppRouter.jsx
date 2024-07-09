513c3df93cd6464969f6156801b6f62dd8074618
CHOI HOWON <cheerhow@naver.com>
2024-07-09 16:23:28 +0900

    design : 온보딩 페이지 퍼블리싱

--- src/pages/AppRouter.jsx
+++ src/pages/AppRouter.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Main from './Main';
 import MainPage from './MainPage';
-import OnBoardingPage from './OnBoardingPage';
 
 function AppRouter() {
   return (
@@ -10,7 +9,6 @@ function AppRouter() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/MainPage" element={<MainPage />} />
-        <Route path="/OnBoarding" element={<OnBoardingPage />} />
       </Routes>
     </Router>
   );
