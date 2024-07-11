1e99b7ec1a04e2c2cbfe0b288f1eff22abd1cc6d
CHOI HOWON <cheerhow@naver.com>
2024-07-12 05:30:37 +0900

    design : Chart 디자인 수정

--- src/pages/ViewChart.jsx
+++ src/pages/ViewChart.jsx
@@ -1,18 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-
-const Profile = styled.button`
-  width: 6rem;
-  height: 2rem;
-  font-size: 1.5rem;
-  color: #ffffff;
-  background-color: rgba(255, 255, 255, 0);
-  border: none;
-  margin-right: 4rem;
-  margin-top: 0rem;
-  position: relative;
-  cursor: pointer;
-`;
+import PropTypes from 'prop-types';
 
 const Statistics = styled.button`
   width: 6rem;
@@ -21,72 +9,63 @@ const Statistics = styled.button`
   color: #ffffff;
   background-color: rgba(255, 255, 255, 0);
   border: none;
-  margin-top: 0rem;
+  margin-left: 2rem;
+  margin-top: 2rem;
   position: relative;
   cursor: pointer;
 `;
 
 const WhiteLine = styled.div`
-  width: 75rem;
-  height: 2px;
-  background-color: #ffffff;
-  margin-top: 3.5rem;
-  margin-left: -18rem;
+  width: 70rem;
+  height: 0.1rem;
+  background-color: #8b8b8bb2;
   position: relative;
+  margin-top: 1rem;
 `;
 
-const GoViewChart = styled.button`
+const GoContainer = styled.div`
+  margin-top: 0.5rem;
+  margin-left: 2rem;
+  position: relative;
+  & > button {
+    margin-right: 1.5rem;
+  }
+`;
+
+const MoveButton = styled.button`
   width: 6rem;
-  height: 2rem;
   font-size: 1rem;
   color: #ffffff;
   background-color: rgba(255, 255, 255, 0);
   border: none;
-  margin-right: 3.5rem;
-  margin-left: -72.8rem;
-  margin-top: 4rem;
   position: relative;
   cursor: pointer;
-`;
-
-const GoAgeChart = styled.button`
-  width: 6rem;
-  height: 2rem;
-  font-size: 1rem;
-  color: #ffffff;
-  background-color: rgba(255, 255, 255, 0);
-  border: none;
-  margin-top: 4rem;
-  margin-right: 3.5rem;
-  position: relative;
-  cursor: pointer;
-`;
-
-const GoCountryChart = styled.button`
-  width: 6rem;
-  height: 2rem;
-  font-size: 1rem;
-  color: #ffffff;
-  background-color: rgba(255, 255, 255, 0);
-  border: none;
-  margin-top: 4rem;
-  position: relative;
-  cursor: pointer;
+  border-bottom: ${(props) =>
+    props.active ? '0.2rem solid rgba(139, 139, 139, 0.7)' : 'none'};
 `;
 
 const ChartContainer = styled.div`
-  display: flex;
+  width: 100%;
+  height: 100%;
   position: relative;
 `;
 
-const StatInformation = styled.div`
-  width: 17rem;
+const ChartandStatContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  margin-top: 4rem;
+  margin-left: 6rem;
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const StatSquare = styled.div`
+  width: 40rem;
   height: 25rem;
   background-color: #36045c;
-  margin-top: 10rem;
-  margin-left: 7rem;
-  border-radius: 2rem;
   position: relative;
+  border-radius: 1rem;
 `;
 
 const UserNickname = styled.p`
@@ -140,38 +119,116 @@ const TotalVideoDB = styled.p`
   position: relative;
 `;
 
-const ChartSpace = styled.div`
-  width: 40rem;
-  height: 25rem;
-  background-color: #cfcfcf;
-  position: relative;
-  margin-top: 10rem;
-  margin-left: -25rem;
-  text-align: center;
+const AlbumContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 1rem;
+  width: 80%;
+  padding-left: 1rem;
+  padding-right: 1rem;
+  margin-top: 1rem;
+  transition: 0.3s;
 `;
+
+const AlbumCoverImage = styled.img`
+  width: 100%;
+  height: 100%;
+  border-radius: 1rem;
+  object-fit: cover;
+`;
+
+const AlbumCover = ({ pic }) => <AlbumCoverImage src={pic} alt="Album Cover" />;
+
+AlbumCover.propTypes = {
+  pic: PropTypes.string.isRequired,
+};
 
 const handleClick = () => {};
 
 const ViewChart = () => {
+  const [activeTab, setActiveTab] = useState('ViewChart');
+
+  const viewChartData = [
+    {
+      pic: 'https://i.ibb.co/Fn93yzJ/1.webp',
+      nickname: 'ViewChart',
+      totalView: '1,000,000',
+      totalVideo: '50',
+    },
+  ];
+
+  const ageChartData = [
+    {
+      pic: 'https://i.ibb.co/cxwzVrb/cat.jpg',
+      nickname: 'AgeChart',
+      totalView: '2,000,000',
+      totalVideo: '70',
+    },
+  ];
+
+  const countryChartData = [
+    {
+      pic: 'https://i.ibb.co/99cZ04Y/4.webp',
+      nickname: 'CountryChart',
+      totalView: '3,000,000',
+      totalVideo: '90',
+    },
+  ];
+
+  const getActiveData = () => {
+    switch (activeTab) {
+      case 'ViewChart':
+        return viewChartData;
+      case 'AgeChart':
+        return ageChartData;
+      case 'CountryChart':
+        return countryChartData;
+      default:
+        return [];
+    }
+  };
+
   return (
     <ChartContainer>
-      <Profile onClick={handleClick}>Profile</Profile>
       <Statistics onClick={handleClick}>My&nbsp;Statistics</Statistics>
       <WhiteLine></WhiteLine>
-      <GoViewChart onClick={handleClick}>View&nbsp;Chart</GoViewChart>
-      <GoAgeChart onClick={handleClick}>Age&nbsp;Chart</GoAgeChart>
-      <GoCountryChart onClick={handleClick}>Country&nbsp;Chart</GoCountryChart>
-      <ChartSpace>차드 들어갈 곳</ChartSpace>
-      <StatInformation>
-        <UserNickname>UserNickname</UserNickname>
-        <InfoLine></InfoLine>
-        <TotalBox>
-          <TotalView>Total View</TotalView>
-          <TotalViewDB>1,000,000</TotalViewDB>
-          <TotalVideo>Total Video</TotalVideo>
-          <TotalVideoDB>50</TotalVideoDB>
-        </TotalBox>
-      </StatInformation>
+      <GoContainer>
+        <MoveButton
+          active={activeTab === 'ViewChart'}
+          onClick={() => setActiveTab('ViewChart')}
+        >
+          View&nbsp;chart
+        </MoveButton>
+        <MoveButton
+          active={activeTab === 'AgeChart'}
+          onClick={() => setActiveTab('AgeChart')}
+        >
+          Age&nbsp;chart
+        </MoveButton>
+        <MoveButton
+          active={activeTab === 'CountryChart'}
+          onClick={() => setActiveTab('CountryChart')}
+        >
+          Country&nbsp;chart
+        </MoveButton>
+      </GoContainer>
+      <AlbumContainer>
+        {getActiveData().map((item, index) => (
+          <ChartandStatContainer key={index}>
+            <AlbumCover pic={item.pic} />
+            <StatSquare>
+              <UserNickname>{item.nickname}</UserNickname>
+              <InfoLine></InfoLine>
+              <TotalBox>
+                <TotalView>Total View</TotalView>
+                <TotalViewDB>{item.totalView}</TotalViewDB>
+                <TotalVideo>Total Video</TotalVideo>
+                <TotalVideoDB>{item.totalVideo}</TotalVideoDB>
+              </TotalBox>
+            </StatSquare>
+          </ChartandStatContainer>
+        ))}
+      </AlbumContainer>
     </ChartContainer>
   );
 };
