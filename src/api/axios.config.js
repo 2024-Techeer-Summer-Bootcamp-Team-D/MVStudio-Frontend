a00a339302ffac42c38a067cbb9b524fd087e208
박유경 <165669386+ukongee@users.noreply.github.com>
2024-07-13 02:17:24 +0900

    chore:개발환경 수정

--- src/api/axios.config.js
+++ src/api/axios.config.js
@@ -1,9 +1,16 @@
 /* eslint-disable no-undef */
 import axios from 'axios';
 
-export const instance = axios.create({
-  baseURL: `${process.env.REACT_APP_BASE_URL}/api/v1`,
+export const jsonAxios = axios.create({
+  baseURL: `${import.meta.env.VITE_REACT_APP_BASE_URL}/api/v1`,
   headers: {
     'Content-Type': 'application/json',
+  },
+});
+
+export const formAxios = axios.create({
+  baseURL: `${import.meta.env.VITE_REACT_APP_BASE_URL}/api/v1`,
+  headers: {
+    'Content-Type': 'multipart/form-data',
   },
 });
