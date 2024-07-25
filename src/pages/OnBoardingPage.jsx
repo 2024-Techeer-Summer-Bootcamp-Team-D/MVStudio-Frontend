8916a897c3ebd94597bf67c92d9ed75003d0da80
CHOI HOWON <cheerhow@naver.com>
2024-07-26 03:31:27 +0900

    [design] 다섯번째섹션 슬라이드 디자인 추가

--- src/pages/OnBoardingPage.jsx
+++ src/pages/OnBoardingPage.jsx
@@ -5,12 +5,31 @@ import { useNavigate } from 'react-router-dom';
 const WholeContainer = styled.div`
   height: 100%;
   width: 100%;
+  position: relative;
+  overflow: hidden;
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: url('https://i.ibb.co/k0wLXnC/Sound-Wave.gif') no-repeat center
+      center fixed;
+    background-size: cover;
+    filter: blur(0.5rem) brightness(50%);
+  }
 `;
 
 const FirstSection = styled.div`
   height: 100%;
   width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   position: relative;
+  gap: 15rem;
 `;
 
 const SecondSection = styled.div`
@@ -20,7 +39,7 @@ const SecondSection = styled.div`
   justify-content: center;
   align-items: center;
   position: relative;
-  gap: 10rem;
+  gap: 8rem;
 `;
 
 const ThirdSection = styled.div`
@@ -30,7 +49,7 @@ const ThirdSection = styled.div`
   justify-content: center;
   align-items: center;
   position: relative;
-  gap: 10rem;
+  gap: 8rem;
 `;
 
 const FourthSection = styled.div`
@@ -44,6 +63,16 @@ const FourthSection = styled.div`
 `;
 
 const FifthSection = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+`;
+
+const SixthSection = styled.div`
   height: 100%;
   width: 100%;
   display: flex;
@@ -108,22 +137,21 @@ const PaginationDot = styled.div`
 
 const Title = styled.div`
   color: white;
-  font-size: 5.5rem;
+  font-size: 6rem;
   position: relative;
   font-weight: 700;
   font-family: 'suit';
-  margin-top: 15rem;
-  margin-left: 12rem;
+  margin-bottom: 17rem;
 `;
 
 const FirstText = styled.div`
   color: white;
-  font-size: 3rem;
+  font-size: 4rem;
   position: relative;
   font-weight: 700;
   font-family: 'suit';
-  margin-left: 55rem;
-  margin-top: 5rem;
+  margin-top: 7rem;
+  filter: brightness(150%);
 `;
 
 const GreyText = styled.div`
@@ -135,33 +163,35 @@ const PurpleText = styled.div`
 `;
 
 const Mac1 = styled.img`
-  width: 45%;
-  height: 50%;
+  width: 40%;
+  height: 45%;
   position: relative;
-  margin-right: 9rem;
+  filter: brightness(150%);
 `;
 
 const SecondText = styled.div`
   color: white;
-  font-size: 3rem;
+  font-size: 3.5rem;
   position: relative;
   font-weight: 700;
   font-family: 'suit';
+  filter: brightness(150%);
 `;
 
 const Mac2 = styled.img`
-  width: 45%;
-  height: 50%;
+  width: 40%;
+  height: 45%;
   position: relative;
+  filter: brightness(150%);
 `;
 
 const ThirdText = styled.div`
   color: white;
-  font-size: 3rem;
+  font-size: 3.5rem;
   position: relative;
   font-weight: 700;
   font-family: 'suit';
-  margin-right: 20rem;
+  filter: brightness(150%);
 `;
 
 const Connect = styled.div`
@@ -172,7 +202,6 @@ const Insta = styled.img`
   width: 35%;
   height: 70%;
   position: relative;
-  margin-right: 12rem;
 `;
 
 const FourthText = styled.div`
@@ -181,6 +210,7 @@ const FourthText = styled.div`
   position: relative;
   font-weight: 700;
   font-family: 'suit';
+  filter: brightness(150%);
 `;
 
 const FifthText = styled.div`
@@ -193,8 +223,7 @@ const FifthText = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  margin-top: 8rem;
-  margin-right: 16rem;
+  margin-top: 6rem;
 `;
 
 const ButtonContainer = styled.div`
@@ -233,6 +262,62 @@ const Button = styled.button`
   }
 `;
 
+const MusicVideoListContainer = styled.div`
+  width: 97%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  justify-content: flex-end;
+  overflow: hidden;
+`;
+
+const scrollLeft = keyframes`
+  0% {
+    transform: translateX(0);
+  }
+  100% {
+    transform: translateX(-100%);
+  }
+`;
+
+const scrollRight = keyframes`
+  0% {
+    transform: translateX(0);
+  }
+  100% {
+    transform: translateX(100%);
+  }
+`;
+
+const MusicVideoList = styled.div`
+  width: 100%;
+  height: 15%;
+  display: flex;
+  gap: 1rem;
+  margin-bottom: 2rem;
+  animation: ${(props) =>
+      props.direction === 'right' ? scrollRight : scrollLeft}
+    30s linear infinite;
+`;
+
+const MusicVideoListBox = styled.img`
+  width: 15rem;
+  height: 9rem;
+  background-color: white;
+  border-radius: 0.4rem;
+  flex-shrink: 0;
+`;
+
+const FifthSectionText = styled.p`
+  font-size: 3rem;
+  color: white;
+  font-family: suit;
+  font-weight: 700;
+  margin-bottom: 8rem;
+  line-height: 1.5;
+`;
+
 const OnBoardingPage = () => {
   const navigate = useNavigate();
   const sectionsRef = useRef([]);
@@ -240,65 +325,106 @@ const OnBoardingPage = () => {
   const [scrollDirection, setScrollDirection] = useState(null);
 
   const scrollToSection = (index) => {
-    sectionsRef.current[index].scrollIntoView({ behavior: 'smooth' });
-    setCurrentSection(index);
+    const section = sectionsRef.current[index];
+    if (section) {
+      const sectionRect = section.getBoundingClientRect();
+      const offset =
+        sectionRect.top +
+        window.pageYOffset -
+        window.innerHeight / 2 +
+        sectionRect.height / 2;
+
+      window.scrollTo({
+        top: offset,
+        behavior: 'smooth',
+      });
+    }
+  };
+
+  const handleScroll = (event) => {
+    if (event.deltaY > 0 && currentSection < sectionsRef.current.length - 1) {
+      setScrollDirection('down');
+      setCurrentSection((prev) => prev + 1);
+    } else if (event.deltaY < 0 && currentSection > 0) {
+      setScrollDirection('up');
+      setCurrentSection((prev) => prev - 1);
+    }
   };
 
   useEffect(() => {
-    const handleScroll = () => {
-      const scrollTop = window.scrollY || window.pageYOffset;
-      const sectionHeight = window.innerHeight;
-      const currentSectionIndex = Math.round(scrollTop / sectionHeight);
-      setScrollDirection(currentSectionIndex > currentSection ? 'down' : 'up');
-      setCurrentSection(currentSectionIndex);
-    };
-
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener('wheel', handleScroll);
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener('wheel', handleScroll);
     };
   }, [currentSection]);
 
   useEffect(() => {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            entry.target.classList.add('visible');
-            setCurrentSection(parseInt(entry.target.dataset.index));
-          }
-        });
-      },
-      { threshold: 0.5 },
-    );
+    scrollToSection(currentSection);
+  }, [currentSection]);
 
-    sectionsRef.current.forEach((section) => {
-      if (section) {
-        observer.observe(section);
-      }
-    });
+  const musicVideos1 = [
+    'https://i.ibb.co/c2q533c/dummy9.webp',
+    'https://i.ibb.co/ysNfRP7/dummy8.webp',
+    'https://i.ibb.co/ck4s16h/dummy7.webp',
+    'https://i.ibb.co/wr0Qf1d/dummy6.webp',
+    'https://i.ibb.co/RjCyvNX/dummy5.webp',
+    'https://i.ibb.co/Bfvtv9S/dummy4.webp',
+    'https://i.ibb.co/zRvDs4n/dummy3.webp',
+    'https://i.ibb.co/8rR2GNY/dummy2.webp',
+    'https://i.ibb.co/XjFTCNZ/dummy1.webp',
+    'https://i.ibb.co/D9152b6/dummy12.webp',
+    'https://i.ibb.co/ZTZ402V/dummy11.webp',
+    'https://i.ibb.co/QQWHDWn/dummy10.webp',
+    'https://i.ibb.co/DkxF5fF/qwd-jpb.webp',
+    'https://i.ibb.co/rm30n24/efm.webp',
+  ];
 
-    return () => {
-      sectionsRef.current.forEach((section) => {
-        if (section) {
-          observer.unobserve(section);
-        }
-      });
-    };
-  }, []);
+  const musicVideos2 = [
+    'https://i.ibb.co/c2q533c/dummy9.webp',
+    'https://i.ibb.co/ysNfRP7/dummy8.webp',
+    'https://i.ibb.co/ck4s16h/dummy7.webp',
+    'https://i.ibb.co/wr0Qf1d/dummy6.webp',
+    'https://i.ibb.co/RjCyvNX/dummy5.webp',
+    'https://i.ibb.co/Bfvtv9S/dummy4.webp',
+    'https://i.ibb.co/zRvDs4n/dummy3.webp',
+    'https://i.ibb.co/8rR2GNY/dummy2.webp',
+    'https://i.ibb.co/XjFTCNZ/dummy1.webp',
+    'https://i.ibb.co/D9152b6/dummy12.webp',
+    'https://i.ibb.co/ZTZ402V/dummy11.webp',
+    'https://i.ibb.co/QQWHDWn/dummy10.webp',
+    'https://i.ibb.co/DkxF5fF/qwd-jpb.webp',
+    'https://i.ibb.co/rm30n24/efm.webp',
+  ];
+
+  const musicVideos3 = [
+    'https://i.ibb.co/c2q533c/dummy9.webp',
+    'https://i.ibb.co/ysNfRP7/dummy8.webp',
+    'https://i.ibb.co/ck4s16h/dummy7.webp',
+    'https://i.ibb.co/wr0Qf1d/dummy6.webp',
+    'https://i.ibb.co/RjCyvNX/dummy5.webp',
+    'https://i.ibb.co/Bfvtv9S/dummy4.webp',
+    'https://i.ibb.co/zRvDs4n/dummy3.webp',
+    'https://i.ibb.co/8rR2GNY/dummy2.webp',
+    'https://i.ibb.co/XjFTCNZ/dummy1.webp',
+    'https://i.ibb.co/D9152b6/dummy12.webp',
+    'https://i.ibb.co/ZTZ402V/dummy11.webp',
+    'https://i.ibb.co/QQWHDWn/dummy10.webp',
+    'https://i.ibb.co/DkxF5fF/qwd-jpb.webp',
+    'https://i.ibb.co/rm30n24/efm.webp',
+  ];
 
   return (
     <WholeContainer>
       <Pagination>
-        {[...Array(5)].map((_, index) => (
+        {[...Array(6)].map((_, index) => (
           <PaginationDot
             key={index}
             active={currentSection === index}
-            onClick={() => scrollToSection(index)}
+            onClick={() => setCurrentSection(index)}
           />
         ))}
       </Pagination>
-      {[...Array(5)].map((_, index) => (
+      {[...Array(6)].map((_, index) => (
         <Section
           key={index}
           data-index={index}
@@ -309,86 +435,103 @@ const OnBoardingPage = () => {
           direction={scrollDirection}
         >
           {index === 0 && (
-            <>
-              <FirstSection>
-                <Title>MVStudio</Title>
-                <FirstText>
-                  <GreyText>
-                    단
-                    <br />
-                    하나뿐인,
-                    <br />
-                  </GreyText>
-                  나만의
+            <FirstSection>
+              <Title>MVStudio</Title>
+              <FirstText>
+                <GreyText>
+                  단
                   <br />
-                  뮤직비디오
+                  하나뿐인,
                   <br />
-                </FirstText>
-              </FirstSection>
-            </>
+                </GreyText>
+                나만의
+                <br />
+                뮤직비디오
+                <br />
+              </FirstText>
+            </FirstSection>
           )}
           {index === 1 && (
-            <>
-              <SecondSection>
-                <SecondText>
-                  <GreyText>당신의 아이디어,</GreyText>
-                  모두 여기에.
-                </SecondText>
-                <Mac1 src="https://i.ibb.co/7NqqQkk/Second-Page-Mac-removebg-preview.png" />
-              </SecondSection>
-            </>
+            <SecondSection>
+              <SecondText>
+                <GreyText>당신의 아이디어,</GreyText>
+                모두 여기에.
+              </SecondText>
+              <Mac1 src="https://i.ibb.co/7NqqQkk/Second-Page-Mac-removebg-preview.png" />
+            </SecondSection>
           )}
           {index === 2 && (
-            <>
-              <ThirdSection>
-                <Mac2 src="https://i.ibb.co/h9R1pCh/qwdwqdqwd-removebg-preview.png" />
-                <ThirdText>
-                  <GreyText>
-                    힙합부터
-                    <br />
-                    재즈까지,
-                  </GreyText>
-                  <Connect>
-                    <PurpleText>유니크한</PurpleText>&nbsp;뮤직비디오
-                  </Connect>
-                </ThirdText>
-              </ThirdSection>
-            </>
+            <ThirdSection>
+              <Mac2 src="https://i.ibb.co/h9R1pCh/qwdwqdqwd-removebg-preview.png" />
+              <ThirdText>
+                <GreyText>
+                  힙합부터
+                  <br />
+                  재즈까지,
+                </GreyText>
+                <Connect>
+                  <PurpleText>유니크한</PurpleText>&nbsp;뮤직비디오
+                </Connect>
+              </ThirdText>
+            </ThirdSection>
           )}
           {index === 3 && (
-            <>
-              <FourthSection>
-                <FourthText>
-                  <Connect>
-                    <GreyText>나만의 뮤직비디오</GreyText>를
-                  </Connect>
-                  <Connect>
-                    <PurpleText>소셜 계정</PurpleText>에
-                  </Connect>
-                  손쉽게 공유
-                </FourthText>
-                <Insta src="https://i.ibb.co/WcQcFpM/qqefmqklem-removebg-preview.png" />
-              </FourthSection>
-            </>
+            <FourthSection>
+              <FourthText>
+                <Connect>
+                  <GreyText>나만의 뮤직비디오</GreyText>를
+                </Connect>
+                <Connect>
+                  <PurpleText>소셜 계정</PurpleText>에
+                </Connect>
+                손쉽게 공유
+              </FourthText>
+              <Insta src="https://i.ibb.co/WcQcFpM/qqefmqklem-removebg-preview.png" />
+            </FourthSection>
           )}
           {index === 4 && (
-            <>
-              <FifthSection>
-                <FifthText>
-                  모든 기능이 준비되어 있으니,
-                  <br /> 지금 한번 시도해 보세요.
-                  <ButtonContainer>
-                    <Button
-                      onClick={() => {
-                        navigate('/auth');
-                      }}
-                    >
-                      Start
-                    </Button>
-                  </ButtonContainer>
-                </FifthText>
-              </FifthSection>
-            </>
+            <FifthSection>
+              <MusicVideoListContainer>
+                <FifthSectionText>
+                  당신을 기다리는
+                  <br />
+                  수많은 뮤직비디오
+                </FifthSectionText>
+                <MusicVideoList direction="left">
+                  {musicVideos1.map((url, idx) => (
+                    <MusicVideoListBox key={idx} src={url} />
+                  ))}
+                </MusicVideoList>
+                <MusicVideoList direction="right">
+                  {[...musicVideos2].reverse().map((url, idx) => (
+                    <MusicVideoListBox key={idx} src={url} />
+                  ))}
+                </MusicVideoList>
+
+                <MusicVideoList direction="left">
+                  {musicVideos3.map((url, idx) => (
+                    <MusicVideoListBox key={idx} src={url} />
+                  ))}
+                </MusicVideoList>
+              </MusicVideoListContainer>
+            </FifthSection>
+          )}
+          {index === 5 && (
+            <SixthSection>
+              <FifthText>
+                모든 기능이 준비되어 있으니,
+                <br /> 지금 한번 시도해 보세요.
+                <ButtonContainer>
+                  <Button
+                    onClick={() => {
+                      navigate('/auth');
+                    }}
+                  >
+                    Start
+                  </Button>
+                </ButtonContainer>
+              </FifthText>
+            </SixthSection>
           )}
         </Section>
       ))}
