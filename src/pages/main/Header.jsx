dd87384f63443fa1836b9e7b5960969405bffffe
j._.jinoo <jinoo0306@naver.com>
2024-07-28 07:52:49 +0900

    feat : 메인페이지 기능 구현 추가

--- src/pages/main/Header.jsx
+++ src/pages/main/Header.jsx
@@ -3,6 +3,15 @@ import styled from 'styled-components';
 import { useQuery } from '@tanstack/react-query';
 import { getList } from '@/api/musicVideos';
 import WhatshotIcon from '@mui/icons-material/Whatshot';
+import { useNavigate } from 'react-router-dom';
+import { IconButton } from '@mui/material';
+import { IconWrapper } from './Main';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import InfoIcon from '@mui/icons-material/Info';
+import VideoItem from './VideoItem';
+import VolumeOffIcon from '@mui/icons-material/VolumeOff';
+import VolumeUpIcon from '@mui/icons-material/VolumeUp';
+import Slider from '@mui/material/Slider';
 
 const BackLayout = styled.div`
   display: flex;
@@ -14,6 +23,10 @@ const BackLayout = styled.div`
   aspect-ratio: 10 / 3;
   height: auto;
   margin-bottom: -7rem;
+
+  &:hover .visible {
+    opacity: 1;
+  }
 `;
 
 const VideoContainer = styled.div`
@@ -67,9 +80,78 @@ const RankComment = styled.div`
   margin-bottom: 0.5rem;
 `;
 
+const OpacityButton = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  opacity: 0;
+  transition: opacity 0.3s ease-in-out;
+  z-index: 8;
+  cursor: pointer;
+`;
+
+const InfoContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  width: 20%;
+  top: 15%;
+  right: 1.5%;
+  /* opacity: ${({ hover }) => (hover ? 1 : 0)};
+  transition: opacity 5s ease-in-out; */
+`;
+
+const VolumeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  top: 5%;
+  right: 4.5%;
+  align-items: center;
+
+  &:hover + .volume-slider {
+    opacity: 1;
+  }
+`;
+
+const StyledVolumeIconButton = styled(IconButton)`
+  &:hover + .volume-slider {
+    opacity: 1;
+  }
+`;
+
+const StyledSlider = styled(Slider)`
+  opacity: 0;
+  transition: opacity 0.3s ease;
+  color: #370080; /* 슬라이더 색상을 보라색으로 변경 */
+  &:hover {
+    opacity: 1;
+  }
+  & .MuiSlider-thumb {
+    color: #370080; /* 슬라이더의 thumb 색상 변경 */
+  }
+
+  & .MuiSlider-track {
+    color: #370080; /* 슬라이더의 track 색상 변경 */
+  }
+
+  & .MuiSlider-rail {
+    color: #370080; /* 슬라이더의 rail 색상 변경 */
+  }
+`;
+
 function Header() {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [infoHover, setInfoHover] = useState(false);
+  const [isMute, setIsMute] = useState(true);
+  const [volume, setVolume] = useState(50);
+
   const videoRefs = useRef([]);
+  const navigate = useNavigate();
 
   const { data } = useQuery({
     queryKey: ['musicVideos'],
@@ -108,8 +190,13 @@ function Header() {
           <VideoWrapper>
             <Video
               src={video.mv_file}
-              muted
-              ref={(el) => (videoRefs.current[index] = el)}
+              muted={isMute}
+              ref={(el) => {
+                videoRefs.current[index] = el;
+                if (el) {
+                  el.volume = volume / 250; // 소리 20%
+                }
+              }}
               autoPlay={index === currentIndex}
             />
             <VideoBlur>
@@ -132,6 +219,99 @@ function Header() {
           </VideoWrapper>
         </VideoContainer>
       ))}
+      <OpacityButton
+        className="visible"
+        onClick={() =>
+          navigate(`/play?id=${data?.music_videos[currentIndex].id}`)
+        }
+      >
+        {/* 볼륨 컨테이너 */}
+        <VolumeContainer onClick={(event) => event.stopPropagation()}>
+          {/* 음소거 on/off 버튼 */}
+          <StyledVolumeIconButton
+            onClick={(event) => {
+              event.stopPropagation();
+              setIsMute((prev) => !prev);
+            }}
+          >
+            {isMute ? (
+              <VolumeOffIcon sx={{ color: 'white' }} />
+            ) : (
+              <VolumeUpIcon sx={{ color: 'white' }} />
+            )}
+          </StyledVolumeIconButton>
+
+          {/* 볼륨 조절 슬라이더 */}
+          <StyledSlider
+            aria-label="Volume"
+            orientation="vertical"
+            valueLabelDisplay="auto"
+            defaultValue={volume}
+            onChange={(event, newValue) => {
+              event.stopPropagation();
+              setVolume(newValue);
+            }}
+            sx={{
+              height: '7rem',
+              opacity: 0,
+            }}
+            className="volume-slider"
+          />
+        </VolumeContainer>
+
+        {/* information 버튼 */}
+        <IconButton
+          style={{ position: 'absolute', top: '5%', right: '1%' }}
+          onMouseEnter={() => setInfoHover(true)}
+          onMouseLeave={() => setInfoHover(false)}
+        >
+          <InfoIcon sx={{ color: 'white' }} />
+        </IconButton>
+
+        {/* 호버시 출력되는 information */}
+        {infoHover && (
+          <InfoContainer hover={infoHover}>
+            <VideoItem video={data?.music_videos[currentIndex]} pageSize={1} />
+          </InfoContainer>
+        )}
+
+        {/* 왼쪽 버튼 */}
+        <IconWrapper>
+          <IconButton
+            sx={{ width: '2.5rem', color: 'white' }}
+            onClick={(event) => {
+              event.stopPropagation();
+              setCurrentIndex((prevIndex) => (prevIndex + 8) % 9);
+            }}
+          >
+            <ArrowForwardIosIcon
+              sx={{
+                width: '100%',
+                height: 'auto',
+                transform: 'scaleX(-1)',
+              }}
+            />
+          </IconButton>
+        </IconWrapper>
+
+        {/* 오른쪽 버튼 */}
+        <IconWrapper>
+          <IconButton
+            sx={{ width: '2.5rem', color: 'white' }}
+            onClick={(event) => {
+              event.stopPropagation();
+              setCurrentIndex((prevIndex) => (prevIndex + 1) % 9);
+            }}
+          >
+            <ArrowForwardIosIcon
+              sx={{
+                width: '100%',
+                height: 'auto',
+              }}
+            />
+          </IconButton>
+        </IconWrapper>
+      </OpacityButton>{' '}
     </BackLayout>
   );
 }
--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -1,14 +1,13 @@
 /* eslint-disable react/display-name */
 /* eslint-disable react/prop-types */
-import React, { useState, useRef, memo, useCallback } from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import { getList } from '@/api/musicVideos';
 import { useInfiniteQuery } from '@tanstack/react-query';
 import { IconButton } from '@mui/material';
-import { defaultProfile } from '@/assets/image';
-import { useNavigate } from 'react-router-dom';
 import Header from './Header';
+import VideoItem from './VideoItem';
 
 const BackLayout = styled.div`
   display: flex;
@@ -51,7 +50,7 @@ const VideoItemList = styled.div`
   scrollbar-width: none; /* Firefox에서 스크롤바 숨기기 */
 `;
 
-const IconWrapper = styled.div`
+export const IconWrapper = styled.div`
   display: flex;
   width: 3%;
   min-width: 3%;
@@ -74,61 +73,6 @@ const ItemArr = styled.div`
     `translateX(calc(-${pageIndex * 100}% - ${pageIndex * 0.4}rem))`};
 `;
 
-const BigVideoItem = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: ${({ size }) => `calc(${100 / size}% - 0.3rem)`};
-  min-width: ${({ size }) => `calc(${100 / size}% - 0.3rem)`};
-  max-width: ${({ size }) => `calc(${100 / size}% - 0.3rem)`};
-  color: white;
-`;
-
-const CoverImage = styled.img`
-  width: 100%;
-  aspect-ratio: 10 / 6; /* 60%의 비율 */
-  object-fit: cover; /* 이미지가 비율을 유지하며 전체를 덮도록 */
-  padding: 0.05rem;
-  border-radius: 0.5rem;
-`;
-
-const ProfileImage = styled.img`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  width: 2.25rem;
-  aspect-ratio: 1 / 1; /* 정사각형 비율을 유지 */
-  overflow: hidden; /* 넘치는 부분을 잘라냄 */
-  border-radius: 100%;
-  margin-right: 0.5rem;
-`;
-
-const VideoHoverContainer = styled.div`
-  display: flex;
-  width: ${({ size }) => `calc(${100 / size}% - 0.3rem)`};
-  min-width: ${({ size }) => `calc(${100 / size}% - 0.25rem)`};
-  height: calc(100% - 3.5rem);
-  min-height: calc(100% - 3.5rem);
-  position: absolute;
-  top: 0;
-  opacity: 0;
-  transition: opacity 1s ease-in-out;
-  &:hover {
-    opacity: 1;
-  }
-`;
-
-const VideoBotInfo = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  height: 2.75rem;
-  position: relative;
-  z-index: 1;
-  color: white;
-  margin-top: 0.5rem;
-`;
-
 const sort = (title) => {
   if (title === 'Top Hits') {
     return 'views';
@@ -141,46 +85,7 @@ const sort = (title) => {
   }
 };
 
-function VideoHover({ src, size }) {
-  const videoRef = useRef(null);
-  const timeoutRef = useRef(null);
-
-  const handleMouseEnter = useCallback(() => {
-    timeoutRef.current = setTimeout(() => {
-      if (videoRef.current) {
-        videoRef.current.load(); // 비디오를 다시 로드하여 초기화
-        videoRef.current.volume = 0.2; // 소리 20%
-        videoRef.current.play();
-      }
-    }, 1000); // 1초 지연
-  }, []);
-
-  const handleMouseLeave = useCallback(() => {
-    clearTimeout(timeoutRef.current);
-    if (videoRef.current) {
-      videoRef.current.pause();
-      videoRef.current.currentTime = 0;
-    }
-  }, []);
-
-  return (
-    <VideoHoverContainer
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-      size={size}
-    >
-      <video
-        ref={videoRef}
-        src={src}
-        preload="auto" // 비디오 미리 로드
-        style={{ width: '100%', height: '100%' }}
-      />
-    </VideoHoverContainer>
-  );
-}
-
 function VideoList({ title }) {
-  const navigate = useNavigate();
   const size = 8;
   const pageSize = title === 'Top Hits' ? 4 : 6;
   const [pageIndex, setPageIndex] = useState(0);
@@ -221,48 +126,16 @@ function VideoList({ title }) {
             />
           </IconButton>
         </IconWrapper>
+
         {/* 비디오 아이템 */}
         <ItemArr pageIndex={pageIndex}>
           {data?.pages.map((page) =>
-            page?.music_videos.map((video, index) => (
-              <BigVideoItem key={index} size={pageSize}>
-                <div
-                  style={{ cursor: 'pointer' }}
-                  onClick={() => navigate(`/play?id=${video.id}`)}
-                >
-                  <CoverImage src={video.cover_image} alt={video.title} />
-                  <VideoHover src={video.mv_file} size={pageSize} />
-                </div>
-                <VideoBotInfo>
-                  <ProfileImage
-                    src={video.profile_image || defaultProfile}
-                    alt="profile"
-                    // onClick={() => navigate(`/users?id=${video.member_id}`)}
-                  />
-                  <div
-                    style={{
-                      display: 'flex',
-                      flexDirection: 'column',
-                      lineHeight: '1.5',
-                      width: '100%',
-                    }}
-                  >
-                    <p
-                      style={{
-                        fontWeight: 'bold',
-                      }}
-                    >
-                      {video.subject}
-                    </p>
-                    <p style={{ fontSize: '0.875rem', color: '#cccccc' }}>
-                      {video.member_name || 'no name user'}
-                    </p>
-                  </div>
-                </VideoBotInfo>
-              </BigVideoItem>
+            page?.music_videos?.map((video, index) => (
+              <VideoItem video={video} pageSize={pageSize} key={index} />
             )),
           )}
         </ItemArr>
+
         {/* 오른쪽 아이콘 */}
         <IconWrapper>
           <IconButton
