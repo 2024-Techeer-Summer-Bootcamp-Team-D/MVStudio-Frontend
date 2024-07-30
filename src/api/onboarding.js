d130e9076f3b9e47820ae284f3d04cbc4e1a8681
CHOI HOWON <cheerhow@naver.com>
2024-07-30 23:58:19 +0900

    [feat] App.js ChildrenWrapper를 온보딩페이지에서 무시하게 수정, api호출 코드 수정, 페이지가 한꺼번에 넘어가지 않게 수정, 배경 속도와 밝기 조절

--- src/api/onboarding.js
+++ src/api/onboarding.js
@@ -1,9 +1,9 @@
-import { jsonAxios } from './axios.config';
+import axios from 'axios';
 
 export const getOnboarding = async (page, size) => {
   try {
-    const response = await jsonAxios.get(
-      `/music-videos/cover-images?&page=${page}&size=${size}`,
+    const response = await axios.get(
+      `${import.meta.env.VITE_REACT_APP_BASE_URL}/api/v1/music-videos/cover-images?&page=${page}&size=${size}`,
     );
 
     console.log('Response data:', response.data);
